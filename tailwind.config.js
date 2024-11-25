5380a83bac97c8690555f3043986825f42658e8d
“GowthamAbi” <gowtham2131ece@gmail.com>
2024-11-25 22:16:44 +0530

    Final Editing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        righteous: ['Righteous', 'sans-serif'], // Add 'Righteous' font
+        Saira: ['"Saira"', "sans-serif"],
+        Rowdies: ['"Rowdies"', "cursive"],
+      },
+    },
   },
   plugins: [],
 }
 
+/*
+font-Saira
+font-righteous
+font-Rowdies
+*/
